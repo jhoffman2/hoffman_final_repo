9af8e0e6436370e344babb01fda5744f1785baaa
Josh Hoffman <jh276903@scc-mmlab-02.local>
2019-05-02 13:03:11 -0400

    Added Whole Project

---
+++ Hoffman_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMotor : MonoBehaviour
+{
+    public float agentSpeed = 5.0f;
+
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+            agent.speed = agentSpeed * Time.deltaTime;
+
+        }
+    }
+}
